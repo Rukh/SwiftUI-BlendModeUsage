9237ce33b5375b7f9a80c49c3241d2a3d4278c95
Gulyagin Dmitry <gulyagin@mail.com>
2023-03-04 14:50:43 +0200

    Initial Commit

---
+++ BlendModeUsage/BlendModeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlendModeUsageApp.swift
+//  BlendModeUsage
+//
+//  Created by Dmitry Gulyagin on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlendModeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
