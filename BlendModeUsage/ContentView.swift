e04411d457ad729cf6f4f9e10a261c16a04670d7
Gulyagin Dmitry <gulyagin@mail.com>
2023-03-04 16:26:40 +0200

    Added slider sample

--- BlendModeUsage/ContentView.swift
+++ BlendModeUsage/ContentView.swift
@@ -8,19 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let lineWidth: CGFloat = 8
+    
+    @State var dragLocation: CGFloat = 0.3
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.green
+            LinearGradient(
+                colors: [
+                    .blue,
+                    .blue.opacity(0.2),
+                ],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            slider
+                .frame(height: lineWidth)
+                .padding()
+                .background(
+                    Image(systemName: "xmark")
+                        .resizable(resizingMode: .tile)
+                        .font(.system(size: 10, weight: .ultraLight))
+                        .opacity(0.3)
+                        .clipShape(Capsule())
+                )
+                .padding()
         }
-        .padding()
+        .foregroundColor(.white)
     }
+    
+    @ViewBuilder
+    private var slider: some View {
+        let coordinateSpace = "slider"
+        let padding: CGFloat = 10
+        GeometryReader { geo in
+            let maxWidth = geo.size.width - padding
+            let dragGesture = DragGesture(
+                coordinateSpace: .named(coordinateSpace)
+            ).onChanged { value in
+                let location = value.location.x / maxWidth
+                dragLocation = max(0, min(location, 1))
+            }
+            Capsule(style: .continuous)
+            ZStack {
+                Circle()
+                    .fill(.black)
+                    .padding(-lineWidth / 2)
+                    .blendMode(.destinationOut)
+                Circle()
+                    .strokeBorder(lineWidth: lineWidth)
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .padding(-padding)
+            .gesture(dragGesture)
+            .offset(x: dragLocation * maxWidth)
+        }
+        .compositingGroup()
+        .coordinateSpace(name: coordinateSpace)
+    }
+    
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(
+            .fixed(width: 480, height: 240)
+        )
     }
 }
+#endif
